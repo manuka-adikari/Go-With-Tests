b17999f94ce2a4496cd51bd86b4a38f50a4e7470
Manuka Adikari <64984096+manuka-adikari@users.noreply.github.com>
2024-11-06 22:32:36 +0530

    interations done

--- iteration/main.go
+++ iteration/main.go
@@ -1,9 +1,9 @@
 package iteration
 
-func Repeat(character string) string {
+func Repeat(character string, expected int) string {
 	var repeated string
-	for i := 0; i < 5; i++ {
-		repeated = repeated + character
+	for i := 0; i < expected; i++ {
+		repeated += character
 	}
 	return repeated
 }
--- iteration/main_test.go
+++ iteration/main_test.go
@@ -2,12 +2,26 @@ package iteration
 
 import "testing"
 
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 5)
+	}
+}
 
+//Practice: Caller can now specify how many times it should repeat the character
+//Added another test case for fun
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
+	repeated_a := Repeat("a", 5)
+	expected_a := "aaaaa"
 
-	if repeated != expected {
-		t.Errorf("expected %q but got %q", expected, repeated)
+	if repeated_a != expected_a {
+		t.Errorf("Test 1 Failed: expected %q but got %q", expected_a, repeated_a)
+	}
+
+	repeated_b := Repeat("b", 4)
+	expected_b := "bbbb"
+
+	if repeated_b != expected_b {
+		t.Errorf("Test 2 Failed: expected %q but got %q", expected_b, repeated_b)
 	}
 }
